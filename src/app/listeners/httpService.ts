7106a3f1b85779dd1c9b15bf98fd3fc72919492d
erkin isci <erkin.isci@matrsikdata.com>
2017-11-10 14:47:08 +0300

    routing auth path is done

--- src/app/listeners/httpService.ts
+++ src/app/listeners/httpService.ts
@@ -1,27 +1,64 @@
 import * as http from 'http';
-
+import * as router from 'router';
 import { Loggy } from '../loggies/loggy';
 import { ILogInfo } from '../models/iLogInfo';
 import { AppConfig } from '../models/appConfig';
+import * as bodyParser from 'body-parser';
+import * as finalHandler from 'finalhandler';
+import * as compression from 'compression';
+
+import { ILogUser } from '../models/authentication/iLogUser';
+import { UserTokenGeneratorService } from '../services/userTokenGeneratorService';
 
 export class HttpService {
 
   private _loggy : Loggy;
-  
+  private _authService : UserTokenGeneratorService;
+
   constructor(private _config: AppConfig) {
-    this._loggy = new Loggy(_config);;
+    this._loggy = new Loggy(_config);
+    this._authService = new UserTokenGeneratorService(_config);
   }
 
   start() {
     let self = this;
 
+    var route = router();
     const server = http.createServer((req, res) => {
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Content-Encoding': 'utf8',
-      });
-      
-      self.processRequest(req, res);
+      route(req, res, finalHandler(req, res));
+    });
+
+    route.use(compression());
+
+    var api = router();
+    route.use('/api/', api);  
+    api.use(bodyParser.json());
+    
+    api.post('/auth', function (req, res) {
+     let logUser : ILogUser;
+     
+     console.log(req.body);     
+     
+     try{
+        logUser = <ILogUser>req.body;       
+      } catch (e){
+        console.log(e);        
+      }       
+
+      if(logUser != null){
+        let token = self._authService.generate(logUser);
+        console.log(token);        
+       
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end(token + '\r\n');
+      }
+      else 
+      {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end('Parametrelerinizi kontrol edin. \r\n');
+      }
     });
 
     server.on('clientError', (err, socket) => {
@@ -31,7 +68,7 @@ export class HttpService {
     server.listen(self._config.connection.port);
   }
 
-  processRequest(req: http.IncomingMessage, res: http.ServerResponse) {
+  handler(req: http.IncomingMessage, res: http.ServerResponse) {
     let self = this;
     let rawData = '';
 
